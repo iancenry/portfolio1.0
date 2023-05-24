39213a7c77ac62f1a73c812ecf162aa2af4470d9
Ian Cenry <iancenry@gmail.com>
2023-05-24 23:49:36 +0300

    update sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import './Sidebar.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '/assets/images/logo-s.png'
 
 const Sidebar = () => (
   <div className="nav-bar">
-    <Link className='logo' to='/'> <img src="" alt="" /> </Link>
+    <Link className='logo' to='/'> <img src={LogoS} alt="" /> </Link>
   </div>
 )
 
 
 export default Sidebar
+
+//10:48 
