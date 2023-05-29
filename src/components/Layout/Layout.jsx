af1b0722213ec853c3977de2458a36c138389609
Ian Cenry <iancenry@gmail.com>
2023-05-29 22:59:15 +0300

    add outlets

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,16 @@
 import './Layout.scss'
 import { Sidebar } from '..'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>
+    <div className='App'>
       <Sidebar />
+      <div className="page">
+        <span className='page__tags page__top-tags'>&lt;body&gt;</span>
+        <Outlet />
+        <span className='page__tags page__bottom-tags'>&lt;body&gt;</span>
+      </div>
     </div>
   )
 }
