08e8e4f96ba1de464d9ed7c7c342c989d59ba493
Ian Cenry <iancenry@gmail.com>
2023-05-30 23:55:11 +0300

    style Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,17 @@
-const Home = () => {
+ import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './Home.scss'
+
+ const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container home-page">
+        <div className="animated-text-zone">
+            <h1>Hi, <br /> I am <img src={LogoTitle} alt="developer" />lobodan <br /> Web Developer </h1>
+            <h2>React Developer / JavaScript Expert</h2>
+            <Link to ='contact' className='flat-btn'>CONTACT ME</Link>
+
+        </div>
+    </div>
   )
 }
 
