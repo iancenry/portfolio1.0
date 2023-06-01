b45ccba44aff2515660e1899ae418e46ed032fe3
Ian Cenry <iancenry@gmail.com>
2023-06-01 21:02:55 +0300

    update About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,17 @@
+import {AnimatedLetters} from '../'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="container about-page">
+        <div className="text-zone">
+            <h1>
+                <AnimatedLetters strArray={[..."About me"]} index={15}/>
+            </h1>
+        </div>
+    </div>
   )
 }
 
 export default About
+
+//50:12
