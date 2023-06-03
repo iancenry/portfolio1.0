af12f5e4d51483bb5036e7984850cb0156ac15f6
Ian Cenry <iancenry@gmail.com>
2023-06-03 21:51:07 +0300

    update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import {AnimatedLetters} from '../'
+import './About.scss'
 
 const About = () => {
   return (
@@ -7,6 +8,9 @@ const About = () => {
             <h1>
                 <AnimatedLetters strArray={[..."About me"]} index={15}/>
             </h1>
+            <p> {"I'm"}  a very ambitious front-end developer looking for a role in an established IT company with the opportunity to work with the latest technologies on challenging and diverse projects.</p>
+            <p> I am quiet confident, naturally curious, and perpetually working on improving my chops one design problem at a time. </p>
+            <p> If I need to define myself in one sentence that would be a family person, father of a beautiful daughter, a sports fanatic, photography enthusiast, and tech-obsessed!!! </p>
         </div>
     </div>
   )
