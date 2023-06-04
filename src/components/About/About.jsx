96036d01efb291b02b740b0cc964bfaa512085a0
Ian Cenry <iancenry@gmail.com>
2023-06-04 23:31:35 +0300

    add cube faces

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import {AnimatedLetters} from '../'
 import './About.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngular } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -18,6 +20,29 @@ const About = () => {
             <p> {"I'm"}  a very ambitious front-end developer looking for a role in an established IT company with the opportunity to work with the latest technologies on challenging and diverse projects.</p>
             <p> I am quiet confident, naturally curious, and perpetually working on improving my chops one design problem at a time. </p>
             <p> If I need to define myself in one sentence that would be a family person, father of a beautiful daughter, a sports fanatic, photography enthusiast, and tech-obsessed!!! </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+            </div>
+          </div>
         </div>
     </div>
   )
