919a6e947cfaf663a45f16440a282c009a63ad70
Ian Cenry <iancenry@gmail.com>
2023-06-04 23:27:08 +0300

    add text animation useEffect

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from 'react'
 import {AnimatedLetters} from '../'
 import './About.scss'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => setLetterClass('text-animate-hover'), 3000)
+  }, [])
+
   return (
     <div className="container about-page">
         <div className="text-zone">
             <h1>
-                <AnimatedLetters strArray={[..."About me"]} index={15}/>
+                <AnimatedLetters letterClass={letterClass} strArray={[..."About me"]} index={15}/>
             </h1>
             <p> {"I'm"}  a very ambitious front-end developer looking for a role in an established IT company with the opportunity to work with the latest technologies on challenging and diverse projects.</p>
             <p> I am quiet confident, naturally curious, and perpetually working on improving my chops one design problem at a time. </p>
